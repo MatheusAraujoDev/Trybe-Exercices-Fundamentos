33adb738bd2bf89e0dda8e577c006edab2a7dfd0
Matheus Araújo <matheusaraujo.dev@gmail.com>
2021-03-26 10:45:58 -0300

    Adiciona exercicios do Bloco 3 Dia 4

---
+++ Fundamentos/Bloco_4/Dia_1/Parte_2/script.js
@@ -0,0 +1,10 @@
+const name = 'Matheus';
+const birthCity = 'Volta Redonda';
+let birthYear = 1997;
+
+// console.log(name, birthCity, birthYear);
+
+birthYear = 2020;
+console.log(birthYear);
+
+birthCity = 'São Paulo'; //Nao altera pois birthCity é uma constante, não uma variável.
