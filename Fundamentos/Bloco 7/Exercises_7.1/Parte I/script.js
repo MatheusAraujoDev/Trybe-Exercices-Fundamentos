11982349273d90c8b16d65fb8febc448ca16f7a0
Matheus Araújo <matheusaraujo.dev@gmail.com>
2021-04-27 09:51:04 -0300

    Adiciona a Parte I dos exercícios do Bloco 7 Dia 1

---
+++ Fundamentos/Bloco 7/Exercises_7.1/Parte I/script.js
@@ -0,0 +1,43 @@
+//Exercício 1 - Faça as modificações necessárias na função comentada para que ela se torne uma arrow function, e também, modifique as concatenações para template literals.
+
+// function testingScope(escopo) {
+//   if (escopo === true) {
+//     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//     console.log(ifScope);
+//   } else {
+//     var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
+// }
+
+// testingScope(true);
+
+const testingScope = escopo => { 
+  if (escopo === true) { 
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+
+testingScope(true);
+
+//Exercício 2 - Utilize template literals para que a chamada console.log(oddsAndEvens); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const sortOddAndEvens = () => {
+  oddsAndEvens[0] = 2
+  oddsAndEvens[1] = 3
+  oddsAndEvens[2] = 4
+  oddsAndEvens[3] = 7
+  oddsAndEvens[4] = 10
+  oddsAndEvens[5] = 13  
+
+  return oddsAndEvens;
+}
+const newArray = sortOddAndEvens();
+console.log(newArray);
